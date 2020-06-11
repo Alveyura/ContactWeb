45dfab7cab00b038e1ff14429e36f309ea9f3489
Simon Verstraete <simon.verstraete9@gmail.com>
2020-06-11 12:05:32 +0200

    Fix mistake + category styling

--- ContactWeb/Controllers/HomeController.cs
+++ ContactWeb/Controllers/HomeController.cs
@@ -31,7 +31,7 @@ namespace ContactWeb.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
